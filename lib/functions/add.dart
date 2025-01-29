1a8ad8a799010fa44d36b5e9ed666d1c67c2f084
Dhruv-M-Sathavara <dhruvms1234@gmail.com>
2025-01-29 23:43:07 +0530

    nothing much

--- lib/functions/add.dart
+++ lib/functions/add.dart
@@ -8,109 +8,131 @@ class Addtask extends StatefulWidget {
 }
 
 class _AddtaskState extends State<Addtask> {
-  bool Income = true,Expense = false , Loan = false;
+  bool Income = true, Expense = false, Loan = false;
+  String selectedCategory = "";
+
+  final List<Map<String, dynamic>> categories = [
+    {"icon": Icons.fastfood, "label": "Food"},
+    {"icon": Icons.people, "label": "Social Life"},
+    {"icon": Icons.pets, "label": "Pets"},
+    {"icon": Icons.directions_car, "label": "Transport"},
+    {"icon": Icons.image, "label": "Culture"},
+    {"icon": Icons.chair, "label": "Household"},
+    {"icon": Icons.checkroom, "label": "Apparel"},
+    {"icon": Icons.brush, "label": "Beauty"},
+    {"icon": Icons.health_and_safety, "label": "Health"},
+    {"icon": Icons.book, "label": "Education"},
+    {"icon": Icons.card_giftcard, "label": "Gift"},
+    {"icon": Icons.category, "label": "Other"},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.white,
-        title: const Text('Task Name',style: TextStyle(fontSize: 18),),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Task Name',
+          style: TextStyle(fontSize: 18),
+        ),
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 15),
+        padding: const EdgeInsets.symmetric(vertical: 15),
         color: Colors.white,
         child: Column(
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              
               children: [
-                categorybutton("Income",Income, Colors.green.shade300),
-                categorybutton("Expense",Expense, Colors.red.shade300),
-                categorybutton("Loan", Loan, Colors.blue.shade300)
+                categorybutton("Income", Income, Colors.green.shade300),
+                categorybutton("Expense", Expense, Colors.red.shade300),
+                categorybutton("Loan", Loan, Colors.blue.shade300),
               ],
             ),
-            SizedBox(height: 25,),
+            const SizedBox(height: 25),
             Container(
-              padding: EdgeInsets.all(20),
-              child: TextField(decoration: InputDecoration(
-                labelText: "Date",
-                labelStyle: TextStyle(fontSize: 15,color: Colors.grey.shade700),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide( width: 1)
-                )
-              ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(decoration: InputDecoration(
-                labelText: "Amount",
-                labelStyle: TextStyle(fontSize: 15,color: Colors.grey.shade700),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(width: 1)
-                )
-              ),),
-            ),
-
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-              child: GestureDetector(
-                onTap: (){
-                  
-                  
-                },
-                child: TextField(decoration: InputDecoration(
-                  labelText: "Category",
-                  labelStyle: TextStyle(fontSize: 15, color:Colors.grey.shade700),
+              padding: const EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Date",
+                  labelStyle:
+                      TextStyle(fontSize: 15, color: Colors.grey.shade700),
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10)
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 1)
-                  )
-                ),),
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(width: 1)),
+                ),
               ),
-            )
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Amount",
+                  labelStyle:
+                      TextStyle(fontSize: 15, color: Colors.grey.shade700),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(width: 1)),
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () {
+                _showCategoryDialog();
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  enabled: false, 
+                  decoration: InputDecoration(
+                    labelText: "Category",
+                    labelStyle: TextStyle(
+                        fontSize: 15, color: Colors.grey.shade700),
+                  
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),),
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(width: 1)),
+                  ),
+                  controller: TextEditingController(
+                      text: selectedCategory), 
+                ),
+              ),
+            ),
           ],
         ),
-      )
+      ),
     );
   }
-  
-Widget categorybutton(String title,bool isSelected,Color color ){
+
+  Widget categorybutton(String title, bool isSelected, Color color) {
     return GestureDetector(
-      onTap: (){
-        if(title == "Income"){
+      onTap: () {
+        if (title == "Income") {
           Income = true;
           Expense = false;
           Loan = false;
-          
         }
-        if(title == "Expense"){
+        if (title == "Expense") {
           Income = false;
           Expense = true;
           Loan = false;
         }
-        if(title == "Loan"){
+        if (title == "Loan") {
           Income = false;
           Expense = false;
           Loan = true;
         }
-        setState(() {
-          
-        });
+        setState(() {});
       },
       child: Material(
         elevation: isSelected ? 5 : 0,
         borderRadius: BorderRadius.circular(20),
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
           decoration: BoxDecoration(
             color: isSelected ? color : Colors.grey.shade200,
             borderRadius: BorderRadius.circular(15),
@@ -126,6 +148,68 @@ Widget categorybutton(String title,bool isSelected,Color color ){
         ),
       ),
     );
+  }
+void _showCategoryDialog() {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Select Category",
+          style: TextStyle(fontSize: 15, color: Colors.blue),
+        ),
+        content: SingleChildScrollView(
+          child: SizedBox(
+            height: 300, 
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, 
+                crossAxisSpacing: 10, 
+                mainAxisSpacing: 10, 
+              ),
+              itemCount: categories.length,
+            
+               
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedCategory = categories[index]['label'];
+                    });
+                    Navigator.of(context).pop(); // 
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          categories[index]['icon'],
+                          size: 32,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        categories[index]['label'],
+                        style: const TextStyle(fontSize: 14),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      );
+    },
+  );
 }
 
 }
